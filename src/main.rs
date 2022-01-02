9a5d1a2823c574c7aff1bc6237623bea0c5c377c
Benjamin Serrano <b.serrano8970@outlook.com>
2022-01-02 09:01:38 -0600

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    homework1();
+}
+
+fn homework1() {
+    fibonacci();
+}
+
+fn fibonacci() {
+    let mut before_prev_sum;
+    let mut previous_sum = 1;
+    let mut current_sum = 0;
+    let mut n = String::new();
+
+    println!("Please enter a number:");
+
+    io::stdin().read_line(&mut n).expect("Unable to read line");
+
+    let n: i32 = n.trim().parse().expect("Number not input");
+    let mut idx = 0;
+
+    while idx < n {
+        print!("{},", current_sum);
+        before_prev_sum = previous_sum;
+        previous_sum = current_sum;
+        current_sum = previous_sum + before_prev_sum;
+
+        idx += 1;
+        println!("");
+    }
+    println!("End of fibonacci sequence");
 }
